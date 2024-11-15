f544411562b5c0866aed6b3e9c857ff2b7c25632
ra4ing <71679158+ra4ing@users.noreply.github.com>
2024-11-15 17:49:53 +0800

    notify docker
    pwn.zsh update for docker hub

--- setup.sh
+++ setup.sh
@@ -27,38 +27,76 @@ echo "======================="
 echo "Installing Anaconda..."
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get -y install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
-echo "Installing Anaconda for Python Management..."
-wget https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh -O "$HOME/anaconda.sh"
+ANACONDA_VERSION=$(curl -s https://repo.anaconda.com/archive/ | grep -oP 'Anaconda3-[0-9]+\.[0-9]+-[0-9]+-Linux-x86_64\.sh' | sort -V | tail -n1)
+wget "https://repo.anaconda.com/archive/$ANACONDA_VERSION" -O "$HOME/anaconda.sh"
 bash "$HOME/anaconda.sh" -b -p "$HOME/.anaconda"
 rm "$HOME/anaconda.sh"
-$HOME/.anaconda/bin/conda conda create -n py310 python=3.10 -y
-$HOME/.anaconda/bin/conda activate py310
+source $HOME/.anaconda/etc/profile.d/conda.sh && \
+$HOME/.anaconda/bin/conda create -n py310 python=3.10 -y && \
+conda activate py310 && \
 pip install --upgrade pip
 
 
 echo "======================="
 echo "Setting zsh"
+sudo apt-get install -y \
+    build-essential \
+    libncursesw5-dev \
+    libssl-dev \
+    libpcre3-dev \
+    libdb-dev \
+    zlib1g-dev \
+    libgdbm-dev \
+    liblzma-dev \
+    uuid-dev \
+    libicu-dev \
+    libxml2-dev \
+    libxslt1-dev \
+    libreadline-dev \
+    libevent-dev \
+    autoconf \
+    yodl \
+    gettext
 
 # cargo
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 # fnm
 curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
-mkdir /run/user/1001/fnm_multishells -p && chmod a+w /run/user/1001/fnm_multishells
-mkdir /run/user/1000/fnm_multishells -p && chmod a+w /run/user/1000/fnm_multishells
+USER_ID=$(id -u)
+sudo mkdir -p /run/user/$USER_ID/fnm_multishells
+sudo chown $USER:$USER /run/user/$USER_ID/fnm_multishells
 
 # thefuck
+pip uninstall thefuck
 pip install https://github.com/nvbn/thefuck/archive/master.zip
 
 #starship
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
-sudo apt-get -y install zsh
+wget https://www.zsh.org/pub/zsh-5.9.tar.xz -O zsh.tar.xz
+tar xf zsh.tar.xz
+cd zsh-5.9
+./configure --prefix=/usr/local --without-tcsetpgrp
+make -j$(nproc)
+sudo make install
+cd ..
+rm -rf zsh-5.9 zsh.tar.xz
+if ! grep -Fxq "/usr/local/bin/zsh" /etc/shells
+then
+    echo "/usr/local/bin/zsh" | sudo tee -a /etc/shells
+fi
+chsh -s /usr/local/bin/zsh
 
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+export RUNZSH=no
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install powerlevel10k
 # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Install gitstatus
 GITSTATUSD_DIR="$HOME/.cache/gitstatus"
 mkdir -p "$GITSTATUSD_DIR"
 wget -O "$GITSTATUSD_DIR/gitstatusd-linux-x86_64.tar.gz" "https://github.com/romkatv/gitstatus/releases/download/v1.5.4/gitstatusd-linux-x86_64.tar.gz"
@@ -66,4 +104,5 @@ tar -xzf "$GITSTATUSD_DIR/gitstatusd-linux-x86_64.tar.gz" -C "$GITSTATUSD_DIR"
 typeset -g POWERLEVEL9K_GITSTATUS_DIR="$HOME/.cache/gitstatusd"
 
 zsh "$HOME/.dotfile/config/zsh/.zshrc"
-echo "ZSH configuration loaded and plugins are downloaded."
+
+echo "Base configuration setup completed successfully!"
--- setup_pwn.sh
+++ setup_pwn.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
+ubuntu_version=$(lsb_release -sr)
+echo "Ubuntu version: $ubuntu_version"
 
 echo "======================="
 echo "flag example..."
-echo "flag{this_is_a_flag}" > /flag && \
-echo "flag{this_is_a_flag}" > /flag.txt && \
-chmod 400 /flag /flag.txt
+sudo sh -c 'echo "flag{this_is_a_flag}" > /flag' && \
+sudo sh -c 'echo "flag{this_is_a_flag}" > /flag.txt' && \
+sudo chmod 400 /flag /flag.txt
 
 echo "======================="
 echo "packages for pwn..."
 sudo dpkg --add-architecture i386
+sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev
 sudo apt-get install -y libc6-dbg libc6-dbg:i386
 sudo apt-get install -y bison flex build-essential gcc-multilib
 sudo apt-get install -y qemu-system-x86 qemu-user qemu-user-binfmt
+sudo apt-get install -y libgmp-dev libmpfr-dev libmpc-dev libreadline-dev libtool 
 sudo apt-get install -y gcc gdb gdbserver gdb-multiarch clang lldb make cmake
-ubuntu_version=$(lsb_release -sr)
-if [[ $(echo "$ubuntu_version >= 20.04" | bc) -eq 1 ]]; then
+sudo apt-get install -y texinfo libncurses5-dev libexpat1-dev libssl-dev libdw-dev libelf-dev
+sudo apt-get install -y libncursesw5-dev libpython3-dev liblzma-dev libbabeltrace-ctf-dev
+sudo apt-get install -y bc lsb-release jq
+
+
+if dpkg --compare-versions "$ubuntu_version" "ge" "20.04"; then
     sudo apt-get install -y libgcc-s1:i386
 else
     sudo apt-get install -y libgcc1:i386
@@ -23,12 +31,12 @@ fi
 
 echo "======================="
 echo "Python packages for pwn..."
-$HOME/.anaconda/bin/conda activate py310 && \
+source $HOME/.anaconda/etc/profile.d/conda.sh && \
+conda activate py310 && \
 pip install --upgrade pip && \
 pip config set global.index-url http://pypi.tuna.tsinghua.edu.cn/simple && \
 pip config set global.trusted-host pypi.tuna.tsinghua.edu.cn && \
 pip install --no-cache-dir \
-python3-dev \
 setuptools \
 pwntools \
 pwncli \
@@ -48,21 +56,41 @@ poetry \
 pipx
 
 echo "======================="
-echo "Setup gdb..."
+echo "Installing gdb..."
 
-# ubuntu_version=$(lsb_release -sr)
-# python_version=$(python3 --version | cut -d ' ' -f 2 | cut -d '.' -f1-2)
-# if [[ "$ubuntu_version" == "18.04" ]]; then
-#     pwndbg_version="2023.07.17"
-# elif [[ "$ubuntu_version" == "16.04" ]]; then
-#     pwndbg_version="2023.07.17"
-# else
-#     pwndbg_version="2024.08.29"
-# fi
-pwndbg_version="2024.08.29"
+if dpkg --compare-versions "$ubuntu_version" "ge" "20.04"; then
+    gdb_version="gdb-15.2"
+elif dpkg --compare-versions "$ubuntu_version" "ge" "18.04"; then
+    gdb_version="gdb-10.2"
+else
+    gdb_version="gdb-8.3"
+fi
+CONDA_PREFIX=$(conda info --base)/envs/py310
+wget https://ftp.gnu.org/gnu/gdb/$gdb_version.tar.gz
+tar -xzf $gdb_version.tar.gz
+cd $gdb_version
+./configure \
+    --prefix=/usr/local \
+    --with-python=$CONDA_PREFIX/bin/python3.10 \
+    LDFLAGS="-L$CONDA_PREFIX/lib -L$CONDA_PREFIX/lib64" \
+    CPPFLAGS="-I$CONDA_PREFIX/include/python3.10 -I$CONDA_PREFIX/include"
+make -j$(nproc)
+sudo make install
+cd ..
+rm -rf $gdb_version $gdb_version.tar.gz
+
+echo "======================="
+echo "installing pwndbg"
+if dpkg --compare-versions "$ubuntu_version" "ge" "20.04"; then
+    pwndbg_version="master"
+else
+    pwndbg_version="2023.07.17"
+fi
 git submodule init && git submodule update
 cd "$HOME/.dotfile/gdb_config/pwndbg"
 git checkout $pwndbg_version
+export PYTHONPATH=$CONDA_PREFIX/lib/python3.10/site-packages
+export PATH=$CONDA_PREFIX/bin:$PATH
 ./setup.sh
-rm "$HOME/.gdbinit"
-ln -s "$HOME/.dotfile/gdb_config/gdbinit" "$HOME/.gdbinit"
+ln -sf "$HOME/.dotfile/gdb_config/gdbinit" "$HOME/.gdbinit"
+echo "PWN configuration setup completed successfully!"
