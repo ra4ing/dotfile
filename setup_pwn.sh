9d8c7cf939bf70b88c5ad9ff190d7b80be673866
ra4ing <ra4ing@gmail.com>
2024-11-16 14:43:37 +0800

    fix gdb and zsh

--- setup_pwn.sh
+++ setup_pwn.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+ubuntu_version=$(lsb_release -sr)
+echo "Ubuntu version: $ubuntu_version"
 
 echo "======================="
 echo "flag example..."
@@ -14,17 +16,14 @@ sudo apt-get install -y libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev
 sudo apt-get install -y libc6-dbg libc6-dbg:i386
 sudo apt-get install -y bison flex build-essential gcc-multilib
 sudo apt-get install -y qemu-system-x86 qemu-user qemu-user-binfmt
-sudo apt-get install -y libgmp-dev libmpfr-dev libmpc-dev 
+sudo apt-get install -y libgmp-dev libmpfr-dev libmpc-dev libreadline-dev libtool 
 sudo apt-get install -y gcc gdb gdbserver gdb-multiarch clang lldb make cmake
 sudo apt-get install -y texinfo libncurses5-dev libexpat1-dev libssl-dev libdw-dev libelf-dev
 sudo apt-get install -y libncursesw5-dev libpython3-dev liblzma-dev libbabeltrace-ctf-dev
 sudo apt-get install -y bc lsb-release jq
-ubuntu_version=$(lsb_release -sr)
-echo "Ubuntu version: $ubuntu_version"
-compare_result=$(echo "$ubuntu_version >= 20.04" | bc)
-echo "Comparison result: $compare_result"
 
-if [[ "$compare_result" -eq 1 ]]; then
+
+if dpkg --compare-versions "$ubuntu_version" "ge" "20.04"; then
     sudo apt-get install -y libgcc-s1:i386
 else
     sudo apt-get install -y libgcc1:i386
@@ -32,7 +31,8 @@ fi
 
 echo "======================="
 echo "Python packages for pwn..."
-source $HOME/.anaconda/bin/activate py310 && \
+source $HOME/.anaconda/etc/profile.d/conda.sh && \
+conda activate py310 && \
 pip install --upgrade pip && \
 pip config set global.index-url http://pypi.tuna.tsinghua.edu.cn/simple && \
 pip config set global.trusted-host pypi.tuna.tsinghua.edu.cn && \
@@ -56,9 +56,15 @@ poetry \
 pipx
 
 echo "======================="
-echo "Setup gdb..."
+echo "Installing gdb..."
 
-gdb_version="gdb-15.2"
+if dpkg --compare-versions "$ubuntu_version" "ge" "20.04"; then
+    gdb_version="gdb-15.2"
+elif dpkg --compare-versions "$ubuntu_version" "ge" "18.04"; then
+    gdb_version="gdb-10.2"
+else
+    gdb_version="gdb-8.3"
+fi
 CONDA_PREFIX=$(conda info --base)/envs/py310
 wget https://ftp.gnu.org/gnu/gdb/$gdb_version.tar.gz
 tar -xzf $gdb_version.tar.gz
@@ -66,25 +72,26 @@ cd $gdb_version
 ./configure \
     --prefix=/usr/local \
     --with-python=$CONDA_PREFIX/bin/python3.10 \
-    LDFLAGS="-L$CONDA_PREFIX/lib" \
-    CPPFLAGS="-I$CONDA_PREFIX/include/python3.10"
+    LDFLAGS="-L$CONDA_PREFIX/lib -L$CONDA_PREFIX/lib64" \
+    CPPFLAGS="-I$CONDA_PREFIX/include/python3.10 -I$CONDA_PREFIX/include"
 make -j$(nproc)
 sudo make install
-rm $gdb_version.tar.gz
-rm -rf $gdb_version
-# ubuntu_version=$(lsb_release -sr)
-# python_version=$(python3 --version | cut -d ' ' -f 2 | cut -d '.' -f1-2)
-# if [[ "$ubuntu_version" == "18.04" ]]; then
-#     pwndbg_version="2023.07.17"
-# elif [[ "$ubuntu_version" == "16.04" ]]; then
-#     pwndbg_version="2023.07.17"
-# else
-#     pwndbg_version="2024.08.29"
-# fi
-pwndbg_version="2024.08.29"
+cd ..
+rm -rf $gdb_version $gdb_version.tar.gz
+
+echo "======================="
+echo "installing pwndbg"
+if dpkg --compare-versions "$ubuntu_version" "ge" "20.04"; then
+    pwndbg_version="master"
+else
+    pwndbg_version="2023.07.17"
+fi
 git submodule init && git submodule update
 cd "$HOME/.dotfile/gdb_config/pwndbg"
 git checkout $pwndbg_version
+pip install -r requirements.txt
+export PYTHONPATH=$CONDA_PREFIX/lib/python3.10/site-packages
+export PATH=$CONDA_PREFIX/bin:$PATH
 ./setup.sh
-rm "$HOME/.gdbinit"
-ln -s "$HOME/.dotfile/gdb_config/gdbinit" "$HOME/.gdbinit"
+ln -sf "$HOME/.dotfile/gdb_config/gdbinit" "$HOME/.gdbinit"
+echo "PWN configuration setup completed successfully!"
