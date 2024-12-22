a5547f43b1f9b9ca63d03574fbffa6960d7eac36
ra4ing <ra4ing@gmail.com>
2024-12-22 11:15:45 +0000

    fix nvm

--- config/zsh/env.zsh
+++ config/zsh/env.zsh
@@ -89,6 +89,7 @@ export XDG_VIDEOS_DIR="$HOME/Videos"
 #nvm
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 export PATH=~/.npm-global/bin:$PATH
 
 ## Comment this to use normal manpager
